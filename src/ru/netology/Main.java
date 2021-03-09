a92ff02440573b0dd95e7d30740400d6ded319ac
Alexandra <rudakalexx@gmail.com>
2021-03-09 16:16:21 +0300

    Change code

--- src/ru/netology/Main.java
+++ src/ru/netology/Main.java
@@ -5,22 +5,22 @@ public class Main {
     public static void main(String[] args) {
 
         Calculator calc = Calculator.instance.get();
-        try {
-            int a = calc.plus.apply(1, 2);
-            int b = calc.minus.apply(1, 1);
-            int c = calc.divide.apply(a, b);
-            calc.println.accept(c);
-        } catch (RuntimeException e) {
-            System.out.println("Division by zero: " + e);
-        }
 
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
-        if (b != 0) {
-            int c = calc.divide.apply(a, b);
-            calc.println.accept(c);
-        } else {
-            System.out.println("Division by zero");
-        }
+        int c = calc.divide.apply(a, b);
+        calc.println.accept(c);
+
+        int d = calc.multiply.apply(a, a);
+        calc.println.accept(d);
+
+        int e = calc.pow.apply(d);
+        calc.println.accept(e);
+
+        int f = calc.abs.apply(-17);
+        calc.println.accept(f);
+
+        System.out.println(calc.isPositive.test(e));
+
     }
 }
