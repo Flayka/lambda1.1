0a44805c94ed2bb7f808db3176b309457d819b46
Alexandra <rudakalexx@gmail.com>
2021-03-10 14:23:26 +0300

    add Readme

--- src/ru/netology/Calculator.java
+++ src/ru/netology/Calculator.java
@@ -3,18 +3,17 @@ package ru.netology;
 import java.util.function.*;
 
 public class Calculator {
-
     static Supplier<Calculator> instance = Calculator::new;
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-
-// BinaryOperator<Integer> divide = (x, y) -> x / y;  // данная запись формулы плоха, т.к. не предусматривает возможное деление на ноль.
-// Что вызовет ошибку RuntimeException.
-// Если отталкиваться от того, что Х/0 = бесконечность, в Java тип double поддерживает бесконечность (Double.POSITIVE_INFINITY),
-// для Integer аналогом будет MAX_VALUE
+    /*
+     BinaryOperator<Integer> divide = (x, y) -> x / y;   данная запись формулы не предусматривает
+     возможное деление на ноль. Что вызовет ошибку RuntimeException.
+     Отталкиваемся от того, что Х/0 = бесконечность. В Java тип double поддерживает бесконечность
+     (Double.POSITIVE_INFINITY), для Integer аналогом будет MAX_VALUE.
+     */
     BinaryOperator<Integer> divide = (x, y) -> y == 0 ? Integer.MAX_VALUE : x / y;
-
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     Predicate<Integer> isPositive = x -> x > 0;
--- src/ru/netology/Main.java
+++ src/ru/netology/Main.java
@@ -3,23 +3,17 @@ package ru.netology;
 public class Main {
 
     public static void main(String[] args) {
-
         Calculator calc = Calculator.instance.get();
-
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
         int c = calc.divide.apply(a, b);
         calc.println.accept(c);
-
         int d = calc.multiply.apply(a, a);
         calc.println.accept(d);
-
         int e = calc.pow.apply(d);
         calc.println.accept(e);
-
         int f = calc.abs.apply(-17);
         calc.println.accept(f);
-
         System.out.println(calc.isPositive.test(e));
     }
 }
